f9d074d0e66e203f10e364f6efd666a88489cd95
Devilking12 <128044704+Devilking12@users.noreply.github.com>
2023-12-09 15:39:49 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@
 BOT_TOKEN = "1158822364:AAEgnhKPvoTbPU5KnO8iMYqQfuF_XcxbBUE"
 
 # Project Name of your Project
-PROJECT_NAME = "GitGram"
+PROJECT_NAME = "Royal family bot"
 
 # If you forked the source code, grab your repo's link and paste it below!
 GIT_REPO_URL = "https://github.com/MadeByThePinsHub/GitGram"
